3ee2de93c017711efb778ec0130a407caecc81e2
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2022-07-06 16:09:08 +0300

    struct ve enum ilerletildi

--- struct-enum.sol
+++ struct-enum.sol
@@ -22,31 +22,69 @@ contract Struct {
     Order[] public orders;
 
 
-    mapping(address => uint) public users;
 
-    function createOrder(string memory _zipCode, uint[] memory _products) public returns(uint) {
+    function createOrder(string memory _zipCode, uint[] memory _products) external returns(uint) {
         require(_products.length > 0, "No products");
 
-        // Order memory order;
-        // order.customer  = msg.sender;
-        // order.zipCode   = _zipCode;
-        // order.products  = _products;
-        // order.status    = Status.Taken;                  Struct oluşturmanın 1. Yolu
-        // orders.push(order);
+        /*Order memory order;
+        order.customer  = msg.sender;
+        order.zipCode   = _zipCode;
+        order.products  = _products;
+        order.status    = Status.Taken;                  Struct oluşturmanın 1. Yolu
+        orders.push(order);
 
 
-        // orders.push(
-        //     Order({
-        //         customer:   msg.sender,
-        //         zipCode:    _zipCode,                    Struct oluşturmanın 2. Yolu
-        //         products:   _products,
-        //         status:     Status.Taken 
-        //     })
-        // );
+        orders.push(
+            Order({
+                customer:   msg.sender,
+                zipCode:    _zipCode,                    Struct oluşturmanın 2. Yolu
+                products:   _products,
+                status:     Status.Taken 
+            })
+        );*/
 
 
         orders.push(Order(msg.sender, _zipCode, _products, Status.Taken)); //Struct oluşturmanın 3. Yolu
 
         return orders.length-1;
+    }
+
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function advenceOrder(uint _orderId) external {
+        require(owner == msg.sender, "You are not a authorized.");
+        require(_orderId < orders.length, "Not a valid order Id");
+        
+        Order storage order = orders[_orderId];
+        require(order.status != Status.Shipped, "Order is already shipped.");
+        
+        if (order.status == Status.Taken) {
+            order.status = Status.Preparing;
+        } else if (order.status == Status. Preparing) {
+            order.status = Status.Boxed;
+        } else if (order.status == Status.Boxed) {
+            order.status = Status.Shipped;
+        }
+    }
+
+
+    function getOrder(uint _orderId) external view returns(Order memory) {
+        require(_orderId < orders.length, "Not a valid order Id");
+        /*
+        Order memory order = orders[_orderId];
+        return order;
+        */
+        return orders[_orderId];
+    }
+
+    function updateZip(uint _orderId, string memory _zip) external{
+        require(_orderId < orders.length, "Not a valid order Id");
+        Order storage order = orders[_orderId];
+        require(order.customer == msg.sender, "You are not the owner of the order.");
+        order.zipCode = _zip; 
     }
 }
